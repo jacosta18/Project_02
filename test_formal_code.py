a545c068e34a55c3249eb0ef82cbbe7fc29f8762
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-08-06 07:24:30 +0100

    Return the digits, pass pytest

--- test_formal_code.py
+++ test_formal_code.py
@@ -1,5 +1,7 @@
 import formal_code
 
+# Using pytest, the following will be tested.
+
 def test_raindrops():
     outcome = formal_code.raindrops(number=30)
     assert outcome == ['Pling', 'Plang']
@@ -15,3 +17,7 @@ def test_02_convert():
 def test_02_convert():
     outcome = formal_code.convert(number=28)
     assert outcome == 'Plong'
+
+def test_03_convert():
+    outcome = formal_code.convert(number=34)
+    assert outcome == '34'
