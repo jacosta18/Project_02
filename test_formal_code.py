589e28d78538b92137e56a7040fd64d47b0426a0
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-08-06 07:20:41 +0100

    test_02: num 28 = 'Plong': pass pytest

--- test_formal_code.py
+++ test_formal_code.py
@@ -8,3 +8,10 @@ def test_convert():
     outcome = formal_code.convert(number=30)
     assert outcome == 'Pling Plang'
 
+def test_02_convert():
+    outcome = formal_code.convert(number=28)
+    assert outcome == 'Plong'
+
+def test_02_convert():
+    outcome = formal_code.convert(number=28)
+    assert outcome == 'Plong'
